de905c32c9833193b3035aedb6926ece9e15b918
RosarioGle <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@github.com>
2024-02-20 14:42:26 +0000

    the last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,17 +15,17 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 6;
-if (n > 5)
+if (n % 10 > 5)
 {
-printf("%d and is greater than 5\n", n);
+printf("Last digit of %d is %d and is less than 6 and not 05\n", n);
 }
-else if (n == 0)
+else if (n % 10 == 0)
 {
-printf("%d and is 0\n", n);
+printf("Last digit of %d is %d and is 0\n", n);
 }
 else
 {
-printf("%d and is less than 6 and not 0\n", n);
+printf("Last digit of %d is %d and is greater than 5\n", n);
 }
 
 return (0);
