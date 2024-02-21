ab2d7065d7fa908aa0bda71797a35edd51535dd6
RosarioGle <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@github.com>
2024-02-21 10:41:46 +0000

    alphaABET

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,9 +11,11 @@ int main(void)
 char letter;
 
 for (letter = 'a'; letter <= 'z'; letter++)
-putchar('\n');
+putchar(letter);
 
 for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
+
 putchar('\n');
 
 return (0);
