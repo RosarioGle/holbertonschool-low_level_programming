1b03a2d4a5296cb5c46e406b4a0b7029d55c842e
RosarioGle <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@github.com>
2024-02-22 14:11:37 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,19 +10,17 @@ int main(void)
 {
 int num;
 
-for (num = 0 < 10; num++;)
-{
-if (num == 9)
-{
-putchar('9');
-}
-else if (num != 9)
+for (num = 0; num < 10; num++)
 {
 putchar(num + '0');
+
+if (num != 9)
+{
 putchar(',');
 putchar(' ');
 }
 }
+putchar('\n');
 
 return (0);
 }
