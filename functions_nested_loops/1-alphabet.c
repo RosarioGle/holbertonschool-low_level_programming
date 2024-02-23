c8d583c4b80c694f464dd7d511556cf0fff315c7
RosarioGle <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@github.com>
2024-02-23 10:02:03 +0000

    I sometimes suffer from insomnia. And when I cant fall asleep, I play what I call the alphabet game

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, in lowercase
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
+_putchar('\n');
+
+return (0);
+}
