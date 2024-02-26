16f44a2188d1040d86bf41c53d6b4f06819515d3
RosarioGle <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@github.com>
2024-02-26 17:51:01 +0000

    I believe in numbers and signs

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* more_numbers - prints 10 time the numbers, from 0 to 14
+*
+* Return: Always 0.
+*/
+void more_numbers(void)
+
+{
+int c, b;
+
+for (c = 0; c <= 9; c++)
+{
+for (b = 0; b <= 14; b++)
+{
+if (c > 9)
+_putchar((b / 10) + '0');
+_putchar((b % 10) + '0');
+}
+_putchar ('\n');
+}
+}
