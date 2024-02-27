7b74a1c02e7d7035b49409ef6a0e474ee57ede75
RosarioGle <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@github.com>
2024-02-27 13:50:41 +0000

    I feel like I am diagonally parked in a parallel universe

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,17 +9,12 @@ void print_diagonal(int n)
 int dia, spc;
 if (n > 0)
 {
-for (dia = 0; dia < n; dia++)
+for (spc = 0; spc < n; spc++)
 {
-for (spc = 0; spc < dia; spc++)
+for (dia = 0; dia < spc; dia++)
 _putchar(' ');
 _putchar('\\');
-
-if (dia == n - 1)
-continue;
-
-_putchar('\n');
-}
 }
 _putchar('\n');
+}
 }
