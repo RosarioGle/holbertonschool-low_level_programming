fcbe72195fa170c41ac938e07dea7466facb59f0
RosarioGle <8824@holbertonstudents.com>
2024-02-28 12:46:30 +0000

    tache 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ while (n < 101)
 if (((n % 3) == 0) && ((n % 5) == 0))
 printf("FizzBuzz");
 else if ((n % 3) == 0)
-printf("Frizz");
+printf("Fizz");
 else if ((n % 5) == 0)
 printf("Buzz");
 else
