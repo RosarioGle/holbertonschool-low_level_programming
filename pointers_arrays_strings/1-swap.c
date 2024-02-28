b7aa8016037f7aaeeba0c1125ad4ae16fb0e7671
RosarioGle <8824@holbertonstudents.com>
2024-02-28 14:48:06 +0000

    tache 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* swap_int - swaps the values of two integers
+*
+* @a:variable a
+* @b:variable b
+*/
+
+void swap_int(int *a, int *b);
+
+{
+int p1 = *a;
+int p2 = *b;
+*a = p2;
+*b =p1;
+}
