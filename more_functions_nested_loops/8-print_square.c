1858b24320e3ea026192f5591feb000432fb347a
RosarioGle <8824@holbertonstudents.com>
2024-02-28 09:16:29 +0000

    tache 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_diagonal - print a diagonal
+*
+* @size: number of times for diagonal
+*/
+
+void print_square(int size)
+{
+if (size <= 0)
+_putchar('\n');
+else
+{
+int i = 0;
+while (i < size)
+{
+int j = 0;
+while (j < size)
+{
+_putchar('#');
+j++;
+}
+_putchar('\n');
+i++;
+}
+}
+}
