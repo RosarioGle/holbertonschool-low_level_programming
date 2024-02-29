d4e225ef1df3ef4ac54090d40ea03f2a60356eca
RosarioGle <8824@holbertonstudent.com>
2024-02-29 14:32:45 +0000

    tache 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* rev_string - reverses a string
+*
+* @s:variable
+*/
+
+void rev_string(char *s)
+{
+int len = 0;
+int i = 0;
+int j = 0;
+char p;
+while (s[len])
+len++;
+for (i = len - 1; i >= len / 2; i--)
+{
+p = s[i];
+s[i] = s[j];
+s[j] = p;
+j++;
+}
+}
