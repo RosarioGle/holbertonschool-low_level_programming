12b5208e2a9a23ad365c3eab3b77290500aee72f
RosarioGle <8824@holbertonstudents.com>
2024-03-01 09:56:13 +0000

    tache 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* *_strncpy - copies a string
+*
+* @dest:variable
+* @src:variable
+* @n:variable
+* Return: Always 0.
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0, len = 0;
+while (src[i++])
+len++;
+for (i = 0; src[i] && i < n; i++)
+dest[i] = src[i];
+for (i = len; i < n; i++)
+dest[i] = '0\n';
+return (dest);
+}
