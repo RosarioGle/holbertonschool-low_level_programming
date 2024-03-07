7db07574c4590f9ca37aa0795e3e76361206d50f
RosarioGle <8824@holbertonstudents.com>
2024-03-07 19:37:09 +0000

    tache 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -16,3 +16,16 @@ return (operation(n, root + 1));
 else
 return (-1);
 }
+
+/**
+* _sqrt_recursion - returns the natural square root of a number.
+* @n:variable
+* Return: Always 0.
+*/
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+return (operation(n, 1));
+}
