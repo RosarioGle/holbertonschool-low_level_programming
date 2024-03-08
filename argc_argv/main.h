3f7e5eda3574043f866dfe40414e03c407f47557
RosarioGle <8824@holbertonstudents.com>
2024-03-08 12:13:04 +0000

    prparation

---
+++ argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef main_h
+#define main_h
+
+int main (int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int _putchar(char c);
+int _atoi(char *s);
+int _isdigit(char *s);
+
+#endif
