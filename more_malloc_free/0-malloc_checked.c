4b322b33adcadfaad101f144440968be26cbc0d6
RosarioGle <8824@holbertonstudents.com>
2024-03-13 13:48:27 +0000

    tache 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* *malloc_checked - allocates memory using
+* @b:variable
+* Return: Always 0.
+*/
+
+void *malloc_checked(unsigned int b)
+{
+void *s;
+
+s = malloc(b);
+if (s == 0)
+exit(98);
+return (s);
+}
