9516f35e38b2aef8f8885ec537a8e853df4d8023
RosarioGle <8824@holbertonstudents.com>
2024-03-20 15:08:00 +0000

    tache 2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings, followed by a new line
+ * @separator: separator
+ * @n: number of parameters
+ * @...: variables parameters
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	char *s;
+	unsigned int a;
+
+	va_start(ap, n);
+	for (a = 0; a < n; a++)
+	{
+		s = va_arg(ap, char *);
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+		if (a < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
