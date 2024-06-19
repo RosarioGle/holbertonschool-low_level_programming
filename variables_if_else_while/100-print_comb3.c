c01e8e8011a062b1a47592e139585810320b4500
RosarioGle <8824@holbertonstudents.com>
2024-06-19 09:23:11 +0000

    creation du fichier 100-print_comb3.c pour le projet C variable

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			int first_digit = i + '0';
+			int second_digit = j + '0';
+
+			putchar(first_digit);
+			putchar(second_digit);
+
+			if (i == 8 && j == 9)
+				putchar('\n');
+			else
+				putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
