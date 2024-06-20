7b41fdd44459b137f131f8a0da515081b8696d01
RosarioGle <8824@holbertonstudents.com>
2024-06-20 13:48:37 +0000

    creation du fichier 104-fibonacci.c pour le projet C function

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -  finds and prints the first 98 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	unsigned long int a = 1, b = 2, c;
+	int count;
+
+	printf("%lu, %lu", a, b);
+	for (count = 2; count < 98; count++)
+	{
+		c = a + b;
+		printf(", %lu", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+	return (0);
+}
