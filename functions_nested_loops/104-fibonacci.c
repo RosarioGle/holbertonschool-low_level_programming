592b63c06765409d603aa8de7db2227c6f14ed4d
RosarioGle <8824@holbertonstudents.com>
2024-06-20 13:53:23 +0000

    correction de la sorti du fichier 104-fibonacci.c

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,23 +1,39 @@
 #include <stdio.h>
 
+#define MAX 98
+#define SPLIT 10000000000
+
 /**
- * main -  finds and prints the first 98 Fibonacci numbers
+ * main - finds and prints the first 98 Fibonacci numbers
  *
  * Return: Always 0
  */
 
 int main(void)
 {
-	unsigned long int a = 1, b = 2, c;
+	unsigned long int a1 = 1, a2 = 0, b1 = 2, b2 = 0;
+	unsigned long int c1, c2;
 	int count;
 
-	printf("%lu, %lu", a, b);
-	for (count = 2; count < 98; count++)
+	printf("%lu, %lu", a1, b1);
+	for (count = 2; count < MAX; count++)
 	{
-		c = a + b;
-		printf(", %lu", c);
-		a = b;
-		b = c;
+		c1 = a1 + b1;
+		c2 = a2 + b2;
+		if (c2 >= SPLIT)
+		{
+			c1 += c2 / SPLIT;
+			c2 = c2 % SPLIT;
+		}
+		printf(", %lu", c1);
+		if (c2 > 0)
+		{
+			printf("%010lu", c2);
+		}
+		a1 = b1;
+		a2 = b2;
+		b1 = c1;
+		b2 = c2;
 	}
 	printf("\n");
 	return (0);
