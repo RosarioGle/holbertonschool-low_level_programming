749244ee08cb910f5ccbd82a8ea7116ca9bf6ca1
RosarioGle <8824@holbertonstudents.com>
2024-06-25 12:51:38 +0000

    creation des fichiers 7-puts_half.c et 7-puts_half pour le projet pointers

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ *
+ * @str: The input string
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int i;
+
+	while (str[length] != '\0')
+	length++;
+	if (length % 2 == 0)
+	i = length / 2;
+	else
+	i = (length - 1) / 2;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
