83f4b277eccb3a921e2a6a4d95cfe9bb76e0e1e2
RosarioGle <8824@holbertonstudents.com>
2024-06-27 08:22:59 +0000

    correction de l'erreur de sorti du fichier 101-print_numbers.c

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -8,15 +8,17 @@
 
 void print_number(int n)
 {
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		print_number(147483648);
+		return;
+	}
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
-	}
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
 	}
 	if (n / 10)
 	print_number(n / 10);
