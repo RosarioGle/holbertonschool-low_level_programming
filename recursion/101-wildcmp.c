e54b0310899e441aa011602ab8d8d3b406cfbac2
RosarioGle <8824@holbertonstudents.com>
2024-07-02 12:37:25 +0000

    creation des fichiers 101-wildcmp.c et 101-wildcmp pour le projet recursion

---
+++ recursion/101-wildcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings and checks
+ * if they can be considered identical.
+ *
+ * @s1: the first string.
+ * @s2: the second string, which can contain the special character '*'.
+ *
+ * Return: 1 if the strings can be considered identical, otherwise 0.
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+	if (*s2 == '*')
+	{
+		if (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)))
+			return (1);
+		return (0);
+	}
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
