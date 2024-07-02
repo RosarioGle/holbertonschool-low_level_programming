b92a94d0ae7f78580c6704e95295ab513d405d3f
RosarioGle <8824@holbertonstudents.com>
2024-07-02 10:13:02 +0000

    creation des fichiers 100-set_string.c et 100-set_string pour le projet even more pointers

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Sets the value of a pointer to a char
+ *
+ * @s: Double pointer to be set
+ * @to: The value to set the pointer to
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
