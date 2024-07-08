ccc331dbaf12b011406a861b550978e8deda7ab1
RosarioGle <8824@holbertonstudents.com>
2024-07-08 12:32:47 +0000

    creation des fichiers 0-create_array.c, a, 1-strdup.c, s, 2-strconcat.c et 2-str_concat pour le projet malloc, free

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ *
+ * @str: The string to duplicate.
+ *
+ * Return: Pointer to the duplicated string, or NULL if insufficient memory
+ * was available or str is NULL.
+ */
+
+char *_strdup(char *str)
+{
+	char *dup_str;
+	unsigned int len = 0, i;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[len])
+		len++;
+	dup_str = malloc(sizeof(char) * (len + 1));
+	if (dup_str == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		dup_str[i] = str[i];
+	dup_str[len] = '\0';
+	return (dup_str);
+}
