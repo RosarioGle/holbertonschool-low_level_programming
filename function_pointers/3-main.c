cc061386583806118ae657bdab0037bc190ebe72
RosarioGle <8824@holbertonstudents.com>
2024-07-15 12:52:37 +0000

    creation des fichiers 3-main.c, 3-op_function.c, 3-get_op_func.c, calc.h et calc pour le projet function pointers

---
+++ function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - check the code
+ *
+ * @argc: numbers of arguments
+ * @argv: arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int (*o)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	o = get_op_func(argv[2]);
+	if (o == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if (num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", o(num1, num2));
+
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - do the sum of the two arguments
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return the sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - do the substraction of the two arguments
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return the substraction of a by b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - do the multiplication of the two arguments
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return the multiplication of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - do the division of the two arguments
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return the division of a by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - do the modulo of the two arguments
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return the modulo of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
