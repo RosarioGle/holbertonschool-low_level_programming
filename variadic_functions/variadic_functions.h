8decb9b40fe61f6aa10f3618f8c7939368f565ee
RosarioGle <8824@holbertonstudents.com>
2024-07-17 12:40:44 +0000

    creation des fichiers 1-print_numbers.c, b et modification du fichier variadic_functions.h pour le projet varaidic functions

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,24 @@
-#ifndef variadic_functions_h
-#define variadic_functions_h
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * struct pr - Struct pr
+ * @pr: Type
+ * @f: The function associated
+ */
+
+typedef struct pr
+{
+	char pr;
+	void (*f)(va_list);
+} pr_t;
 
 int sum_them_all(const unsigned int n, ...);
-int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
