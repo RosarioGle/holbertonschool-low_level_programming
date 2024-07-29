1ff1de827d826587381b36388c7ad0c205c02a71
RosarioGle <8824@holbertonstudents.com>
2024-07-29 13:07:32 +0000

    creation des fichiers 7-puts_half.c et 7-puts_half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int a;
+
+	while (str[len])
+		len++;
+	a = len / 2;
+	if ((len % 2) != 0)
+	a = a + 1;
+	while (a < len)
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
