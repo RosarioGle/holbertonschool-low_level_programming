a6f65292887a667bc9fff11371fde5769c788a4c
RosarioGle <8824@holbertonstudents.com>
2024-07-31 08:37:29 +0000

    creation des fichiers 2-strncpy.c et 2-strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - copie a string
+ *
+ * @dest: destination
+ * @src: source
+ * @n: number of byte of src
+ *
+ * Return: copy string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int cpy = 0, len = 0;
+
+	while (src[cpy++])
+		len++;
+	for (cpy = 0; src[cpy] && cpy < n; cpy++)
+		dest[cpy] =src[cpy];
+	for (cpy = len; cpy < n; cpy++)
+		dest[cpy] = '\0';
+	return (dest);
+}
