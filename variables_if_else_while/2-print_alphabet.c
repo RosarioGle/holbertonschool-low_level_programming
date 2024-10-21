7e2a4edac9d45fd251d1f8b6641b5572a1a9a716
RosarioGle <8824@holbertonstudents.com>
2024-10-21 08:30:19 +0000

    creation du fichier 2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
