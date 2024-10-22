ac10542f5adab8e4e0703df398fbf5f189a1c13e
RosarioGle <8824@holbertonstudents.com>
2024-10-22 08:24:38 +0000

    creation du fichier 6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @n: number
+ *
+ * Return: n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
