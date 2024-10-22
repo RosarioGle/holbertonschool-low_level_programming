716c795954138e8699c539f56d93f5eb0ab7f696
RosarioGle <8824@holbertonstudents.com>
2024-10-22 10:41:20 +0000

    creation du fichier 9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+
+void times_table(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if ((a * b) < 10)
+			{
+				if (b > 0)
+				{
+					_putchar(32);
+					_putchar((a * b) + 48);
+				}
+				else
+					_putchar((a * b) +48);
+			}
+			else
+			{
+				_putchar((a * b) / 10 + 48);
+				_putchar((a * b) % 10 + 48);
+			}
+			if (b < 9)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+		}
+		_putchar('\n');
+	}
+}
