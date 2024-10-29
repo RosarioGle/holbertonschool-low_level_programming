254a69a3c28d6f1b2cea6f9b34f417ed5cb0dcb7
RosarioGle <8824@holbertonstudents.com>
2024-10-29 09:55:04 +0000

    creation du fichier 5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: the string
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	int a, c;
+	char cpy;
+
+	while (s[len])
+		len++;
+	for (a = len - 1; a >= len / 2; a--)
+	{
+		cpy = s[a];
+		s[a] = s[c];
+		s[c] = cpy;
+		c++;
+	}
+}
