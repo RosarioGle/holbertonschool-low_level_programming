1daca68ff4c7b58527b6da43881a4c5e10b35377
RosarioGle <8824@holbertonstudents.com>
2024-10-31 11:36:16 +0000

    creation du fichier 7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ *
+ * @s: the string
+ *
+ * Return: the pointer c
+ */
+
+char *leet(char *s)
+{
+	int a, b;
+	char letter[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char encode[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (a = 0; s[a]; a++)
+	{
+		for (b = 0; b < 11; b++)
+		{
+			if (s[a] == letter[b])
+				s[a] = encode[b];
+		}
+	}
+	return (s);
+}
+
