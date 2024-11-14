0eb3923d5f8176e6dbd1c9a1ff7954f4bc3c9cbf
RosarioGle <8824@holbertonstudents.com>
2024-11-14 09:51:19 +0000

    creation du fichier 2-print_dog.c et ajout de son prototype dans le fichier dog.h

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct dog
+ *
+ * @d: structure dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		d->name = "(nli)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner %s\n", d->owner);
+}
