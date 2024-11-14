25520093cb9dbb392cb9aab986ea1ebfc02c8948
RosarioGle <8824@holbertonstudents.com>
2024-11-14 09:57:50 +0000

    correction des erreur de sorti du fichier 2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,9 +14,10 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		d->name = "(nli)";
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		d->owner = "(nil)";
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner %s\n", d->owner);
+	else printf("Owner %s\n", d->owner);
 }
