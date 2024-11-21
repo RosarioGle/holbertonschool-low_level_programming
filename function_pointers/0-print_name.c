fe5954c4088b3b11f45fa72fff13ff55dd3caf27
RosarioGle <8824@holbertonstudents.com>
2024-11-21 08:17:31 +0000

    ajout d'une condition dans le fichier 0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -5,9 +5,13 @@
  *
  * @name: the name
  * @f: the functions
+ *
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
