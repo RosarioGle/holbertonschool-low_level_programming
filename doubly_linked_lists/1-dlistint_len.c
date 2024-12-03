a0284da28a2bfe9c36b8cc6b330489ce1a8fd077
RosarioGle <8824@holbertonstudents.com>
2024-12-03 12:41:51 +0000

    creation du fichier 1-dlistint_len.c

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - returns the number of elements in the linked list
+ *
+ * @h: the list
+ *
+ * Return: the number of elements int the linked list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		size++;
+		h = h->next;
+	}
+	return (size);
+}
